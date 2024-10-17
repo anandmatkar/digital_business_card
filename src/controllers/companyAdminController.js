e90053f65671979ce28444e2c95efc1554be0cdd
Chetangit123 <pchetan839@gmail.com>
2024-10-17 10:52:27 +0530

    changes in addCompanyDetails api

--- src/controllers/companyAdminController.js
+++ src/controllers/companyAdminController.js
@@ -750,8 +750,8 @@ module.exports.addCompanyDetails = async (req, res) => {
 
       let insertCompanyDetails = await connection.query(s1, [
         id,
-        mysql_real_escape_string(company_name),
-        mysql_real_escape_string(company_email.toLowerCase()),
+        (company_name),
+        (company_email.toLowerCase()),
         description,
         company_address,
         company_logo,
