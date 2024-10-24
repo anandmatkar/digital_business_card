b823dda3eee16d5dbfcc31ff39aa9145bc0dd4a1
Chetangit123 <pchetan839@gmail.com>
2024-10-24 16:32:31 +0530

    import my escape

--- src/utils/dbscript.js
+++ src/utils/dbscript.js
@@ -1,3 +1,5 @@
+const { mysql_real_escape_string } = require("./helpers");
+
 const db_sql = {
   Q0: `SELECT id,name,email,password,avatar FROM super_admin WHERE  deleted_at IS NULL`,
   Q1: `SELECT id,name,email,password,avatar FROM super_admin WHERE email = '{var1}' AND deleted_at IS NULL`,
